b37c9732e65c4580d9f535cbc0ffb9da88a4b003
Amanda <amandagabylopes23@gmail.com>
2023-05-25 12:39:11 -0300

    doxygen e makefile

--- src/matrizes/matrizes.c
+++ src/matrizes/matrizes.c
@@ -4,28 +4,31 @@
 
 /*Função de alocação dinâmica*/
 Complexo **aloca_matriz(int linhas, int colunas) {
-    Complexo **matriz = malloc(linhas* sizeof(Complexo*)); // procura lugar na memoria para armazenar o vetor linhas
+     // Aloca memoria para o vetor de ponteiros 'matriz'
+    Complexo **matriz = malloc(linhas* sizeof(Complexo*));  // Cada elemento do vetor 'matriz' será um ponteiro para uma linha da matriz
     for (int i = 0; i < linhas; i++) {
-        matriz[i] = malloc(colunas* sizeof(Complexo)); // procura lugar na memoria para armazenar o vetor colunas
+        matriz[i] = malloc(colunas* sizeof(Complexo)); // Para cada linha, aloca memoria para um vetor de 'colunas' elementos do tipo Complexo
     }
+
+    // Retorna o ponteiro para a matriz alocada
     return matriz;
 }
 
 
 
-/*Função gerar numero aleatorio*/
+/* Função gerar numero aleatorio */
 int geraNum(Complexo **matriz, int linhas, int colunas){
-    //gera numero aleatorio
+    //gera numero aleatorio  para preencher a matriz
     for(int i=0; i <linhas; i++){
         for(int j=0; j<colunas; j++){
-            matriz[i][j].real = rand() % 10;
-            matriz[i][j].imag = rand() % 10;
+            matriz[i][j].real = rand() % 10; // Atribui um valor aleatorio entre 0 e 9 para a parte real
+            matriz[i][j].imag = rand() % 10; // Atribui um valor aleatorio entre 0 e 9 para a parte imaginaria
         }
     }
         return 0;
 }
 
-/*Função imprimir 1 operando*/
+/* Função imprimir uma matriz */
 int imprime1(Complexo **matriz1, int linhas, int colunas){
     //imprime na tela a primeira matriz
     printf("Operando: A=\n");
@@ -35,11 +38,11 @@ int imprime1(Complexo **matriz1, int linhas, int colunas){
         }
     printf("\n");
     }
-        return 0;
+            return 0;
 }
 
 
-/*Função imprimir 2 operandos*/
+/* Função imprimir duas matrizes */
 int imprime2(Complexo **matriz1, Complexo **matriz2, int linhas, int colunas){
     //imprime na tela a primeira matriz
     printf("Operando: A=\n");
@@ -49,7 +52,7 @@ int imprime2(Complexo **matriz1, Complexo **matriz2, int linhas, int colunas){
         }
     printf("\n");
     }
-        return 0;
+
 
     //imprime na tela a segunda matriz
     printf("\nOperando: B=\n");
@@ -59,6 +62,7 @@ int imprime2(Complexo **matriz1, Complexo **matriz2, int linhas, int colunas){
         }
     printf("\n");
     }
+        return 0;
 }
 
 
@@ -72,13 +76,14 @@ int imprimRes(Complexo **resultado,int linhas,int colunas){
         }
     printf("\n");
     }
-        return 0;
+            return 0;
+
 }
 
 /*Função de operação soma*/
 
 Complexo **somaMatriz(int linhas, int colunas){
-    //aloca na memoria as matrizes soma e resultado
+    // somaMatriz recebe como parametro tipos inteiros linha e colunas
     Complexo **matriz1 = aloca_matriz(linhas, colunas);
     Complexo **matriz2 = aloca_matriz(linhas, colunas);
     Complexo **resultado = aloca_matriz(linhas,colunas);
@@ -134,25 +139,26 @@ Complexo **subtraMatriz(int linhas, int colunas){
 
 /*Função de Operação Transposta*/
 Complexo **transpostaMatriz(int linhas, int colunas){
-    //aloca na memoria a matriz e a transposta
+    // aloca na memória a matriz transposta e o resultado
     Complexo **matriztrans = aloca_matriz(linhas, colunas);
     Complexo **resultado = aloca_matriz(linhas, colunas);
-    //gera numero aleatorio
+    //gera numero aleatorio para a matriz transposta
     geraNum(matriztrans, linhas, colunas);
 
-    //operação da matriz transposta
+    // realiza a operação da matriz transposta
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             resultado[i][j].real= matriztrans[j][i].real;
             resultado[i][j].imag= matriztrans[j][i].imag;
         }
     }
-    //
+    // Imprime a matriz transposta e o resultado da operação
 
     printf("\n======Teste da operacao transposta======\n");
     imprime1(matriztrans, linhas, colunas);
     imprimRes(resultado, linhas, colunas);
 
+   //retorna o resultado da operação
     return resultado;
 }
 
@@ -160,54 +166,54 @@ Complexo **transpostaMatriz(int linhas, int colunas){
 /*Função de operação conjugada*/
 
 Complexo **conjugMatriz(int linhas, int colunas){
-    //aloca matrizes
+    //aloca matrizes matrizconj (matriz original) e resultado (matriz conjugada)
     Complexo **matrizconj = aloca_matriz(linhas, colunas);
     Complexo **resultado = aloca_matriz(linhas, colunas);
-    //gera numero aleatorio
+    //gera numeros aleatorios para a matriz matrizconj
     geraNum(matrizconj, linhas, colunas);
 
-    //operação da matriz conjugada
+    // realiza a operação da matriz conjugada
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             resultado[i][j].real= matrizconj[i][j].real;
             resultado[i][j].imag= -matrizconj[i][j].imag;
         }
     }
-    //
+    //Imprime a matriz matrizconj e o resultado da operação
 
     printf("\n======Teste da operacao conjugada======\n");
     imprime1(matrizconj, linhas, colunas);
     imprimRes(resultado, linhas, colunas);
 
-
+ // Retorna o resultado da operação
     return resultado;
 }
 
 
-/*Função de operação hermitiana*/
+/* Função de operação hermitiana */
 Complexo **hermitMatriz(int linhas, int colunas){
-    //aloca matrizes
+    // aloca as matrizes matrizhermit (matriz original), matriz (matriz transposta) e resultado (matriz conjugada)
     Complexo **matrizhermit = aloca_matriz(linhas, colunas);
     Complexo **matriz = aloca_matriz(linhas, colunas);
     Complexo **resultado = aloca_matriz(linhas, colunas);
-    //gera numeros aleatorios
+    //gera numeros aleatorios para a matriz matrizhermit
     geraNum(matrizhermit, linhas, colunas);
 
-    //operação da matriz transposta
+    // realiza a operação da matriz transposta
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             matriz[i][j].real= matrizhermit[j][i].real;
             matriz[i][j].imag= matrizhermit[j][i].imag;
         }
     }
-    //operação da matriz conjugada
+    // realiza a operação da matriz conjugada
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             resultado[i][j].real= matriz[i][j].real;
             resultado[i][j].imag= -matriz[i][j].imag;
         }
     }
-    //
+    // imprime a matriz matrizhermit, a matriz transposta e a matriz conjugada
     //Operação Hermitiana
     printf("\n======Teste da operacao hermitiana======\n");
     //imprime matriz hermitiana
@@ -217,6 +223,7 @@ Complexo **hermitMatriz(int linhas, int colunas){
     printf("\nConjugada: ");
     imprime1(resultado, linhas, colunas);
 
+    // Verifica se a matriz e hermitiana comparando a matriz original e a matriz conjugada
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             if(matrizhermit[i][j].real == resultado[i][j].real && matrizhermit[i][j].imag == resultado[i][j].imag){
@@ -229,21 +236,23 @@ Complexo **hermitMatriz(int linhas, int colunas){
         }
     }
 
+    // Retorna a matriz resultado
      return resultado;
 }
 
 
 /*Função de operação produto escalar*/
 Complexo **prodEscMatriz(int linhas, int colunas){
-    //aloca matrizes
+    //aloca as matrizes matrizprod1, matrizprod2 (matrizes de entrada) e resultado (matriz resultante)
+    Complexo **matrizprod1 = aloca_matriz(linhas, colunas);
     Complexo **matrizprod1 = aloca_matriz(linhas, colunas);
     Complexo **matrizprod2 = aloca_matriz(linhas, colunas);
     Complexo **resultado = aloca_matriz(linhas, colunas);
-    //gera numeros aleatorios
+    //gera numeros aleatorios  para as matrizes matrizprod1 e matrizprod2
     geraNum(matrizprod1, linhas, colunas);
     geraNum(matrizprod2, linhas, colunas);
 
-    //operação do produto escalar
+    // realiza a operação do produto escalar
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             resultado[i][j].real = 0;
@@ -255,27 +264,29 @@ Complexo **prodEscMatriz(int linhas, int colunas){
         }
     }
 
-
+    // imprime as matrizes matrizprod1, matrizprod2 e o resultado da operação
     printf("\n======Teste da operacao produto escalar======\n");
     imprime2(matrizprod1, matrizprod2, linhas, colunas);
-    imprimRes(matrizprod1, linhas, colunas);
+    imprimRes(resultado, linhas, colunas);
 
+    // retorna o resultado da operação
     return resultado;
 }
 
 
 /*Função de operação produto matricial*/
 Complexo **prodMatMatriz(int linhas, int colunas){
-    //aloca matrizes
+    //aloca matrizes matrizmat1, matrizmat2 (matrizes de entrada), resultado (matriz resultante) e cont (matriz auxiliar para cálculos intermediários)
     Complexo **matrizmat1 = aloca_matriz(linhas, colunas);
     Complexo **matrizmat2 = aloca_matriz(linhas, colunas);
+    Complexo **resultado = aloca_matriz(linhas, colunas);
     Complexo **cont = aloca_matriz(linhas, colunas);
     int k;
-    //gera numeors aleatorios
+    //gera numeors aleatorios  para as matrizes matrizmat1 e matrizmat2
     geraNum(matrizmat1, linhas, colunas);
     geraNum(matrizmat2, linhas, colunas);
 
-    //operação de produto matricial
+    //realiza a operação de produto matricial
     for (int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             for(k=0; k<colunas; k++){
@@ -286,18 +297,19 @@ Complexo **prodMatMatriz(int linhas, int colunas){
     }
 
 
-
+    // imprime as matrizes matrizmat1, matrizmat2 e o resultado da operação
     printf("\n======Teste da operacao produto matricial======\n");
     imprime2(matrizmat1, matrizmat2, linhas, colunas);
     imprimRes(cont, linhas, colunas);
 
-
+    // retorna o cont(resultado) da operação
     return cont;
 }
 
 
 /*Função teste todos*/
 Complexo **todosMatriz(int linhas, int colunas){
+     // Chamando as funções de teste para cada operação
     somaMatriz(linhas, colunas);
     subtraMatriz(linhas, colunas);
     transpostaMatriz(linhas, colunas);
@@ -305,6 +317,6 @@ Complexo **todosMatriz(int linhas, int colunas){
     hermitMatriz(linhas, colunas);
     prodEscMatriz(linhas, colunas);
     prodMatMatriz(linhas, colunas);
-
+    
     return 0;
 }
